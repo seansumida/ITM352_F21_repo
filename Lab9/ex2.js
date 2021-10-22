537810a18ce39dc0b59b00eaa31daf7de0c14295
seansumida <60155464+seansumida@users.noreply.github.com>
2021-10-21 23:49:24 -1000

    lab9

--- Lab9/ex2.js
+++ Lab9/ex2.js
@@ -10,3 +10,20 @@ while(stepCount++ <16){
     }
 }
 }
+stepCountBorder=1;
+while(stepCountBorder++ <16){
+    step =
+    controller.move(); {
+    if(!step) {
+        controller.rotate();
+        controller.move();
+        }
+    }
+}
+controller.rotate();
+stepCountInside=1
+while(stepCountInside++ <4){
+        controller.move();
+        
+
+}
